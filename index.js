acbc8c197bd1538f360283ffb577a88d49b0578f
Farley <kfbfarley@outlook.com>
2021-01-19 21:16:21 +0000

    jsdocs

--- index.js
+++ index.js
@@ -5,18 +5,29 @@ const handlebars = require('handlebars');
 const nodemailer = require("nodemailer");
 const config = require("./config/index")
 
-
+/**
+ * Current available methods:
+ * * Send(template, receiver)
+ */
 module.exports = () =>{
     let transporter = nodemailer.createTransport({
         host: config.SES.SERVER,
         port: config.SES.PORT,
-        secure: false,
+        secure: true,
         auth: {
           user: config.SES.KEY,
           pass: config.SES.SECRET
         }
     });
     return {
+        /**
+         * 
+         * @param {*} template Is a given object of the name and type.
+         * * HTML: {"name": "confirm", "type": "html"}
+         * * TEXT: {"name": "confirm", "type": "text"}
+         * @param {*} receiver Is a given object of the receiver email, email subject and your templates variables.
+         * @returns A promise.
+         */
         Send: async (template, receiver) =>{           
             return await transporter.sendMail({
                 from: config.SES.FROM,
